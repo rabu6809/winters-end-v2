d02df03c05adb42b402d3843a1abadeb550d9679
rabu6809 <133813254+rabu6809@users.noreply.github.com>
2024-08-08 10:55:04 -0500

    fin battle

--- winters-end.py
+++ winters-end.py
@@ -1,12 +1,15 @@
 from random import *
+
+
 class protagonist:
-    def __init__ (self, perk):
-        self.hp  = 100
+
+    def __init__(self, perk):
+        self.hp = 100
         self.dmgLight = 5
         self.dmgHeavy = 10
         self.cripple = 0
         self.fracture = 0
-        self.bleed  = 0
+        self.bleed = 0
         self.artery = 0
         self.i4s = 0
         self.meleeDefenseMulti = 1
@@ -19,24 +22,25 @@ class protagonist:
             self.rangeDefenseMulti = 1.2
             self.dmgLight = 6
             self.dmgHeavy = 12
+
     def debuffProt(self, effectID):
-          if effectID == 1:
-              self.bleed = 30
-              print("You're bleeding...")
-          elif effectID == 2:
-              self.artery = 20
-              print("Your arteries hurt...")
-          elif effectID == 3:
-              self.cripple = 45
-              print ("Your leg is broken. You can't roam far or flee.")
-          elif effectID == 4:
-              self.fracture = 45
-              print("Your arm is broken. You can't use Shove.")
-          elif effectID == 5:
-              self.dmgLight = self.dmgLight + 1
-              self.dmgHeavy = self.dmgHeavy + 2
-              print("Honorable applied")
-    
+        if effectID == 1:
+            self.bleed = 30
+            print("You're bleeding...")
+        elif effectID == 2:
+            self.artery = 20
+            print("Your arteries hurt...")
+        elif effectID == 3:
+            self.cripple = 45
+            print("Your leg is broken. You can't roam far or flee.")
+        elif effectID == 4:
+            self.fracture = 45
+            print("Your arm is broken. You can't use Shove.")
+        elif effectID == 5:
+            self.dmgLight = self.dmgLight + 1
+            self.dmgHeavy = self.dmgHeavy + 2
+            print("Honorable applied")
+
     def displayStats(self):
         print(f"Player - {player.hp}/100 HP")
         textBuffer = ""
@@ -49,13 +53,13 @@ class protagonist:
         if self.artery > 0:
             textBuffer = textBuffer + f"[ARTERY {str(self.artery)}]     "
         print(textBuffer)
-    
+
     def damageProt(self, dmg):
         self.hp = self.hp - dmg
         print(f"You took {str(dmg)} damage.")
         if self.hp <= 0:
             self.isDead = True
-    
+
     def turn(self):
         if self.cripple > 0:
             self.cripple = self.cripple - 1
@@ -79,7 +83,9 @@ class protagonist:
         if self.hp <= 0:
             self.isDead = True
 
+
 class hostileHuman:
+
     def __init__(self, hp, light, heavy, shove, parry, ranged):
         self.hp = hp
         self.lightDamage = light
@@ -89,23 +95,30 @@ class hostileHuman:
         self.ranged = ranged
         self.maxHp = hp
         self.parry = False
+
     def displayStats(self):
         print(f"Enemy - HP {self.hp}/{self.maxHp}")
+
     def beginParry(self):
         self.parry = True
         print("The enemy is parrying.")
+
     def takeShove(self):
         self.parry = False
         self.stunned = True
+
     def takeDamage(self, amount):
         if self.parry == False:
-            self.hp = self.hp - round(amount*self.resistmulti)
+            self.hp = self.hp - round(amount * self.resistmulti)
             print(f"The enemy took {round(amount*self.resistmulti)} damage.")
         elif self.parry:
             print("Enemy blocked the attack ")
             self.parry = False
+
+
 def battle(name, hp, lightDamage, heavyDamage, canShove, canParry, isRanged):
-    enemy = hostileHuman(HP, lightDamage, heavyDamage, canShove, canParry, isRanged)
+    enemy = hostileHuman(HP, lightDamage, heavyDamage, canShove, canParry,
+                         isRanged)
     print(f"{name} blocks your way.")
     while enemy.hp > 0:
         player.displayStats()
@@ -126,7 +139,7 @@ flee - REQUIRES STUNNED ENEMY attempt to flee battle""")
         elif action == "lightc":
             enemy.takeDamage(player.dmgLight)
             enemy.takeDamage(player.dmgLight)
-            enemy.takeDamage(round(player.dmgLight*1.5))
+            enemy.takeDamage(round(player.dmgLight * 1.5))
             turns = turns - 3
         elif action == "heavy":
             enemy.takeDamage(player.dmgHeavy)
@@ -138,6 +151,7 @@ flee - REQUIRES STUNNED ENEMY attempt to flee battle""")
             shove = True
             turns = 0
 
+
 print("Welcome to Winter's End")
 print("""Status effects:
 FRACTURE disables shoves.
@@ -148,7 +162,8 @@ input("Enter to continue.")
 print("""Choose your perk.
 1 - ARTILLERIST has better ranged performance but is weak at melee.
 2 - EXECUTIONER has powerful melee performance but suffers from issues with ranged enemies.
-Leave Blank - SURVIVALIST doesn't have any special quirk. The recommended beginner class.""")
+Leave Blank - SURVIVALIST doesn't have any special quirk. The recommended beginner class."""
+      )
 
 perkChoice = input("Input perk ID: ")
 if perkChoice == "":
@@ -162,4 +177,12 @@ player.damageProt(15)
 player.displayStats()
 night = 1
 while player.isDead == False:
-    print("hi")
+    area = input("what area do you want to go to?")
+    if area == "castle":
+        if player.cripple:
+            print("You can't go there.")
+    elif area == "tower":
+        if player.cripple:
+            print("You can't go there.")
+    elif area == "mountain":
+        battle("Lead Pipe Scav", 50, 3, 5, False, False, False)
